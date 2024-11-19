e2ec782a4af426e7de1503a011d188d436d135d9
Jihyun Oh <68390559+import-max@users.noreply.github.com>
2024-11-19 10:22:30 +0900

    add case

---
+++ programmers/120822.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    result = my_string[-1]
+    idx = -2
+    while len(result) < len(my_string):
+        result += my_string[idx]
+        idx -=1
+    return result
+
+solution("abcde")
